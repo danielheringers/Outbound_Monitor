6902c5b3270c210ba147b9129cec76517f4379e2
Daniel Heringer <danielheringers@gmail.com>
2024-10-26 14:03:45 -0300

    Fonts

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -79,6 +79,50 @@ const StatusBadge: React.FC<StateStatus> = ({ name, status }) => {
   );
 };
 
+const StatusBadgeGeral: React.FC<StateStatus> = ({ name, status }) => {
+  const getStatusColor = (status: Status) => {
+    switch (status) {
+      case "online":
+        return "bg-green-500";
+      case "inconsistent":
+        return "bg-yellow-500";
+      case "offline":
+        return "bg-red-500";
+    }
+  };
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Badge
+            variant="outline"
+            className="flex items-center gap-2 cursor-pointer text-[16px]"
+          >
+            {name}
+            <span className="relative flex h-3 w-3">
+              <span
+                className={`animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 ${getStatusColor(
+                  status
+                )}`}
+              ></span>
+              <span
+                className={`relative inline-flex rounded-full h-3 w-3 ${getStatusColor(
+                  status
+                )}`}
+              ></span>
+            </span>
+          </Badge>
+        </TooltipTrigger>
+        <TooltipContent side="top" align="center" className="p-2 rounded-md">
+          <p>{status.charAt(0).toUpperCase() + status.slice(1)}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+
 export function Footer() {
   const [generalStatuses, setGeneralStatuses] = useState<StateStatus[]>([]);
   const [stateStatuses, setStateStatuses] = useState<StateStatus[]>([]);
@@ -95,13 +139,10 @@ export function Footer() {
         }
         const data = await response.json();
 
-        console.log("Received data:", JSON.stringify(data, null, 2));
-
         if (!Array.isArray(data)) {
           if (typeof data === "object" && data !== null) {
             const possibleArray = Object.values(data).find(Array.isArray);
             if (possibleArray) {
-              console.log("Found array in object:", possibleArray);
               processData(possibleArray);
               return;
             }
@@ -149,13 +190,13 @@ export function Footer() {
 
   return (
     <footer className="border-t">
-      <div className="px-4 pb-4">
+      <div className="pb-4">
         <div className="flex flex-col w-full">
-          <div className="flex flex-row w-full pt-2 justify-between">
+          <div className="flex flex-row w-full pt-2 justify-between px-4">
             <h3 className="text-lg font-semibold">Sefaz</h3>
             <div className="flex flex-wrap gap-2">
               {generalStatuses.map((state) => (
-                <StatusBadge
+                <StatusBadgeGeral
                   key={state.name}
                   name={state.name}
                   status={state.status}
@@ -165,7 +206,7 @@ export function Footer() {
           </div>
           <Separator className="my-2" />
           <div>
-            <div className="flex flex-wrap gap-2">
+            <div className="flex flex-wrap gap-2 justify-center px-4">
               {stateStatuses.map((state) => (
                 <StatusBadge
                   key={state.name}
