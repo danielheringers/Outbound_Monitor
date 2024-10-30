7bdb479a69d08fc0b84e65df840707e1e7a94f12
Daniel Heringer <danielheringers@gmail.com>
2024-10-30 07:50:57 -0300

    pwdir

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { login } from '@/services/authService';
 
 const AUTO_LOGIN_USERNAME = process.env.AUTO_LOGIN_USERNAME || '';
-const AUTO_LOGIN_PASSWORD = process.env.AUTO_LOGIN_PASSWORD || '';
+const AUTO_LOGIN_PASSWORD = 'U2FsdGVkX18wdMex2cArMVVhEioZ9qzH/ADFZFIw5rg='
 const TV_IDENTIFIER = 'TV_ppwsy6v6r';
 
 export async function POST(request: NextRequest) {
